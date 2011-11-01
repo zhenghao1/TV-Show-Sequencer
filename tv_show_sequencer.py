62b225cd38b5a3a54942635a953569841e6cb6f6
Mark Huang <zhenghao12@gmail.com>
2011-11-02 00:40:10 +0800

    First commit

---
+++ tv_show_sequencer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+DESC = """
+This script renames TV shows by providing: 
+    show,
+    season,
+    episode,
+"""
+
+USAGE = """
+Usage: %prog <options> FILE1 (FILE2 FILE3 FILE 4...)
+"""
+
+VERSION = """
+%prog version 1.0
+"""
